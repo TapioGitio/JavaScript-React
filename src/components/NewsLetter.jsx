da64d29cb671ae23b70177a6b46208604c656a65
Janne <tapioninetytwo@live.se>
2024-10-23 15:15:59 +0200

    Added some functions to validate the subsribe

--- src/components/NewsLetter.jsx
+++ src/components/NewsLetter.jsx
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function NewsLetter() {
+
+    const [email, setEmail] = useState('')
+    const [error, setError] = useState(null)
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault()
+        if(!email) {
+            setError('Please write your email-address')
+        } else {
+            console.log('Email sent:', email)
+        }
+    }
+
+    const handleChange = (e) => {
+        setEmail(e.target.value)
+        setError(null)
+    }   
+
   return (
     <section className="newsletter container">
         <div className="news-container">
@@ -11,16 +30,20 @@ function NewsLetter() {
                 <div className="news-title">
                     <h2>Subscribe to our
                     newsletter </h2>
+                    {
+                        error && <small style={{ color: 'red'}}>{error}</small>
+                    }
                 </div>
             </div>
 
             <div className="container">                   
-                <form className="email-form">
-                    <input className="form-input" type="email" name="email" id="email" required placeholder ="Your email" />
+                <form className="email-form" onSubmit={handleSubmit}>
+                    <input onChange={handleChange} value={email} className="form-input" type="email" name="email" id="email"  placeholder ="Your email" />
                     <button type="submit" className="btn-sub">Subscribe</button>
+                   
                 </form>
             </div>
-            
+
         </div>
     </section>
   )
