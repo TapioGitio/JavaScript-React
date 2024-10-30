ba31736763d8ea43cd13055d4e3c9b6d5103bac0
Janne <tapioninetytwo@live.se>
2024-10-30 12:04:20 +0100

    Added error handling in the contact form made some changes in index to be able to show a succces message

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,12 +3,24 @@ import React, { useState } from 'react'
 const ContactForm = () => {
 
   const [contactFormData, setContactFormData] = useState({ fullName: "", email: "", specialist: ""})
+  const [errors, setErrors] = useState({})
   const [success, setSuccess] = useState(false)
   
   const handleSubmit = async (e) => {
 
     e.preventDefault()
 
+      const newErrors =  {}
+      Object.keys(contactFormData).forEach(field => {
+        if(contactFormData[field].trim() === '') {
+          newErrors[field] = `Please enter your ${field}.`
+        }
+      })
+
+      if(Object.keys(newErrors).length > 0) {
+        setErrors(newErrors)
+        return
+      }
  
       const res = await fetch ('https://win24-assignment.azurewebsites.net/api/forms/contact', {
         method: 'POST',
@@ -35,6 +47,14 @@ const ContactForm = () => {
   const handleChange = (e) => {
     const { name, value } = e.target
     setContactFormData({...contactFormData, [name]: value})
+
+    if(value.trim() === '') {
+
+      setErrors(prevErrors => ({...prevErrors, [name]: `Please enter your ${name}.`}))
+    } else {
+      setErrors(prevErrors => ({...prevErrors, [name]: ''}))
+    }
+
   }
 
   if(success) {
@@ -57,17 +77,17 @@ const ContactForm = () => {
         <div className="input-group">
             <label className="form-label">Full Name</label>
             <input type="text" name='fullName' value={contactFormData.fullName} required className="form-input" placeholder=' e.g: Henrik Svensson' onChange={handleChange}/>
-            <small></small>
+            <small className='error'>{errors.fullName && errors.fullName}</small>
         </div>
         <div className="input-group">
             <label className="form-label">Email Address</label>
             <input type="email" name='email' value={contactFormData.email} required className="form-input" placeholder=' e.g: henrik@domain.com' onChange={handleChange}/>
-            <small></small>
+            <small className='error'>{errors.email && errors.email}</small>
         </div>
         <div className="input-group">
             <label className="form-label">Specialist</label>
             <input type="text" name='specialist' value={contactFormData.specialist} required className="form-input" placeholder=' e.g: Therapist, Doctor, etc' onChange={handleChange}/>
-            <small></small>
+            <small className='error'>{errors.specialist && errors.specialist}</small>
         </div>
 
         <button className="btn-primary btn-appoint">Make an appointment</button>
