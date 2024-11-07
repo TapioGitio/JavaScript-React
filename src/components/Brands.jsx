88cb5bb0e86e27fc6c46118aa3e3616662fbc157
Janne <tapioninetytwo@live.se>
2024-11-07 11:37:42 +0100

    changed the branditems to be an array and no longer a state

--- src/components/Brands.jsx
+++ src/components/Brands.jsx
@@ -5,14 +5,14 @@ import BrandItem from './BrandItem'
 function Brands() {
 
 
-    const [brandItems, setBrandItems] = useState([
+    const brandItems = [
         { id: 'brand-1', logo: './images/logo-icon-1.svg', altText: 'Logo ipsum logo 1'},
         { id: 'brand-2', logo: './images/logo-icon-2.svg', altText: 'Logo ipsum logo 2'},
         { id: 'brand-3', logo: './images/logo-icon-3.svg', altText: 'Logo ipsum logo 3'},
         { id: 'brand-4', logo: './images/logo-icon-4.svg', altText: 'Logo ipsum logo 4'},
         { id: 'brand-5', logo: './images/logo-icon-5.svg', altText: 'Logo ipsum logo 5'},
         { id: 'brand-6', logo: './images/logo-icon-6.svg', altText: 'Logo ipsum logo 6'},
-    ])
+    ]
 
   return (
     <section id="brands">
