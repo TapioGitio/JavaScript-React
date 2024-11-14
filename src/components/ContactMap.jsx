c48cfad8a9f9a7dc1eea012d5f41d466db7ba80c
Janne <tapioninetytwo@live.se>
2024-11-14 10:22:17 +0100

    changed the map to a hospital in washington, and added the correct address as the map shows

--- src/components/ContactMap.jsx
+++ src/components/ContactMap.jsx
@@ -6,18 +6,15 @@ const ContactMap = () => {
       <div className="container">
         <div className="map-wrapper">
           <div className="map">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d70181.20259462584!2d12.726743888557094!3d56.65426292427672!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4651bcc50131eda3%3A0xee6ad95d3ac91235!2sHalmstad%20City%20Airport!5e0!3m2!1ssv!2sse!4v1729166766314!5m2!1ssv!2sse"
-              allowFullScreen=""
-              loading="lazy"
-              referrerPolicy="no-referrer-when-downgrade"
-            ></iframe>
+          <iframe 
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d49609.38802450802!2d-77.18067169189456!3d39.00192687128298!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b7cbe77baa5f41%3A0x54579d527c71c71f!2sSuburban%20Hospital!5e0!3m2!1ssv!2sse!4v1731575301880!5m2!1ssv!2sse" loading="lazy">
+          </iframe>
           </div>
 
           <div className="map-info">
             <div className="map-1">
               <h2 className="mb-1">Medical Center 1</h2>
-              <p className="mb-1"><i className="fa-solid fa-location-dot"></i>4517 Washington Ave. Manchester, Kentucky 39495</p>
+              <p className="mb-1"><i className="fa-solid fa-location-dot"></i>8600 Old Georgetown Rd, Bethesda</p>
               <p className="mb-1"><i className="fa-solid fa-phone"></i>(406) 555-0120</p>
               <p><i className="fa-solid fa-clock"></i><b>Mon – Fri</b>: 9:00 am – 22:00 am </p>
               <p className="m-l"><b> Sat – Sun</b>: 9:00 am – 20:00 am</p>
