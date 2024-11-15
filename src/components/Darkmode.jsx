443be76d995ee53f208af8efda6e27c3099a63f8
Janne <tapioninetytwo@live.se>
2024-11-15 18:09:59 +0100

    corrected a naming error in darkmode

--- src/components/Darkmode.jsx
+++ src/components/Darkmode.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 
-function Darkmode({DarkTheme, toggleDark}) {
+function Darkmode({darkTheme, toggleDark}) {
 
 
   return (
     <div className="btn-toggle-switch">
         <span className="label">Dark Mode</span>
         <label htmlFor="darkmode-switch" className="toggle-switch">
-            <input id="darkmode-switch" type="checkbox" checked={DarkTheme} onChange={toggleDark} />
+            <input id="darkmode-switch" type="checkbox" checked={darkTheme} onChange={toggleDark} />
             <span className="slider round"></span>
         </label>
     </div>
